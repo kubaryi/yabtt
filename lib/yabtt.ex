5f2d9416efed27c00df725fac4a8ba1db04fcf7d
Mogeko <zhengjunyi@live.com>
2022-11-06 17:09:44 +0000

    build: setup the basic development environment

---
+++ lib/yabtt.ex
@@ -0,0 +1,18 @@
+defmodule YaBTT do
+  @moduledoc """
+  Documentation for `YaBTT`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> YaBTT.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/yabtt_test.exs
@@ -0,0 +1,8 @@
+defmodule YaBTTTest do
+  use ExUnit.Case
+  doctest YaBTT
+
+  test "greets the world" do
+    assert YaBTT.hello() == :world
+  end
+end
