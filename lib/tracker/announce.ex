889561d062722015287317b5c0defa1e87ba1219
Mogeko <zhengjunyi@live.com>
2022-12-16 02:32:42 +0100

    docs: add the list of params and explain of return

--- lib/tracker/announce.ex
+++ lib/tracker/announce.ex
@@ -8,8 +8,13 @@ defmodule Tracker.Announce do
 
   This function pass the passed options `call/2`.
 
-  params:
-    opts - The options passed to the plug.
+  ## Parameters
+
+  - opts: The options passed to the plug.
+
+  ## Return
+
+  The options passed to the plug.
   """
   @spec init(Plug.opts()) :: Plug.opts()
   def init(opts), do: opts
@@ -23,12 +28,18 @@ defmodule Tracker.Announce do
   - Return peers who hold the target torrent
   - Return other information
 
-  params:
-    conn - The connection struct.
-    opts - The options passed to the plug.
+  ## Parameters
+
+  - conn: The connection struct.
+  - opts: The options passed to the plug.
+
+  ## Return
+
+  Processed connection structure.
   """
   @spec call(Plug.Conn.t(), Plug.opts()) :: Plug.Conn.t()
   def call(conn, _opts) do
+    # TODO: Implement the announce logic
     conn
   end
 end
