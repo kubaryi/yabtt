f6abe63f99464392684c966045e1635b219666b7
Mogeko <zhengjunyi@live.com>
2022-12-16 02:22:18 +0100

    test: supplement docs for anounce

--- lib/tracker/announce.ex
+++ lib/tracker/announce.ex
@@ -2,11 +2,33 @@ defmodule Tracker.Announce do
   @moduledoc false
 
   @behaviour Plug
-  import Plug.Conn
 
+  @doc """
+  Initializes the plug. This function is called once when the plug is compiled.
+
+  This function pass the passed options `call/2`.
+
+  params:
+    opts - The options passed to the plug.
+  """
+  @spec init(Plug.opts()) :: Plug.opts()
   def init(opts), do: opts
 
+  @doc """
+  The main entry point for the plug. This function is called for every request.
+
+  This function is responsible for:
+  - Receive a report from peer
+  - Update the track list
+  - Return peers who hold the target torrent
+  - Return other information
+
+  params:
+    conn - The connection struct.
+    opts - The options passed to the plug.
+  """
+  @spec call(Plug.Conn.t(), Plug.opts()) :: Plug.Conn.t()
   def call(conn, _opts) do
-    :pass
+    conn
   end
 end
