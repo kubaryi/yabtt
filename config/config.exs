1aa6052003b2e17972044cc6ce2c8b465ddbd160
Mogeko <zhengjunyi@live.com>
2022-12-19 03:00:06 +0100

    build: explain the meaning of the compile config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,8 @@
 import Config
 
 config :yabtt, YaBTT.Database.Cache,
+  # The name for the cache table (ETC), default to :yabtt_database_cache
   ets_name: :yabtt_database_cache,
-  # if the ETS start with `:set`, you will only get the last peer
+  # The options for the cache table (ETC), default to [:bag, :named_table, :protected]
+  # If the ETS start with `:set`, you will only get the last peer.
   ets_opts: [:bag, :named_table, :protected]
