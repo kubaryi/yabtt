eed78354e119036bf1314ac3d2dcc3780b3cce8e
Mogeko <zhengjunyi@live.com>
2022-12-19 02:29:38 +0100

    build: set default ets_name to :yabtt_database_cache

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
 config :yabtt, YaBTT.Database.Cache,
-  ets_name: :cache_ets_bag,
+  ets_name: :yabtt_database_cache,
   # if the ETS start with `:set`, you will only get the last peer
   ets_opts: [:bag, :named_table, :protected]
