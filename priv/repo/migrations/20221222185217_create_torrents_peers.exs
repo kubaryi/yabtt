a3e0ccea901d2e7cc3e1fa92fdcf5ef8fa6643cf
Mogeko <zhengjunyi@live.com>
2022-12-22 21:35:23 +0100

    build: never auto gen primary_key for torents_peers

--- priv/repo/migrations/20221222185217_create_torrents_peers.exs
+++ priv/repo/migrations/20221222185217_create_torrents_peers.exs
@@ -2,7 +2,7 @@ defmodule YaBTT.Repo.Migrations.CreateTorrentsPeers do
   use Ecto.Migration
 
   def change do
-    create table(:torrents_peers) do
+    create table(:torrents_peers, primary_key: false) do
       add :info_hash, references(:torrents)
       add :peer_id, references(:peers)
     end
