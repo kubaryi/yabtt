0dd66fc008841af936b245a82a1951edf24076d4
Mogeko <zhengjunyi@live.com>
2022-12-23 19:03:57 +0100

    feat: adjust database

--- priv/repo/migrations/20221222185217_create_torrents_peers.exs
+++ priv/repo/migrations/20221222185217_create_torrents_peers.exs
@@ -3,10 +3,10 @@ defmodule YaBTT.Repo.Migrations.CreateTorrentsPeers do
 
   def change do
     create table(:torrents_peers, primary_key: false) do
-      add :info_hash, references(:torrents)
+      add :torrent_id, references(:torrents)
       add :peer_id, references(:peers)
     end
 
-    create unique_index(:torrents_peers, [:info_hash, :peer_id])
+    create unique_index(:torrents_peers, [:torrent_id, :peer_id])
   end
 end
