b1ab00147ce7a0bfa196a855c579ca93c1ec5455
Mogeko <zhengjunyi@live.com>
2022-12-25 00:52:54 +0100

    feat: allow the link table to have the primary key

--- priv/repo/migrations/20221222185217_create_torrents_peers.exs
+++ priv/repo/migrations/20221222185217_create_torrents_peers.exs
@@ -2,7 +2,7 @@ defmodule YaBTT.Repo.Migrations.CreateTorrentsPeers do
   use Ecto.Migration
 
   def change do
-    create table(:torrents_peers, primary_key: false) do
+    create table(:torrents_peers) do
       add :torrent_id, references(:torrents)
       add :peer_id, references(:peers)
     end
