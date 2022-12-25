e823f483725a12674139ebb4cd31eddf8e84deb2
Mogeko <zhengjunyi@live.com>
2022-12-25 18:27:44 +0100

    test; add some tests for YaBTT.Schema.Params

--- lib/yabtt/schema/params.ex
+++ lib/yabtt/schema/params.ex
@@ -1,5 +1,13 @@
 defmodule YaBTT.Schema.Params do
-  @moduledoc false
+  @moduledoc """
+  The schema for validating the request parameters.
+
+  We only focus on if the required parameters `info_hash` and `peer_id` are
+  present. We don't care about the values of these parameters.
+
+  This is an independent embedded schema, witch means all the checking
+  happens in the memory and no database operations are involved.
+  """
 
   use Ecto.Schema
   import Ecto.Changeset
@@ -14,7 +22,27 @@ defmodule YaBTT.Schema.Params do
   @type changeset_t :: Ecto.Changeset.t(t())
   @type params :: map()
 
-  @doc false
+  @doc """
+  Creates a changeset based on the `struct` and `params`.
+
+  In most cases, `struct` is a `%YaBTT.Schema.Params{}` and the `params`
+  is the request parameters.
+
+  ## Parameters
+
+  - `struct`: the changeset struct
+  - `params`: the request parameters
+
+  ## Examples
+
+      iex> params = %{"info_hash" => "f0a15e27fafbffc1c2f18f69fcac2dfa461ff4e8", "peer_id" => "-TR14276775888084598"}
+      iex> YaBTT.Schema.Params.changeset(%YaBTT.Schema.Params{}, params)
+      #Ecto.Changeset<action: nil, changes: %{info_hash: \"f0a15e27fafbffc1c2f18f69fcac2dfa461ff4e8\", peer_id: \"-TR14276775888084598\"}, errors: [], data: #YaBTT.Schema.Params<>, valid?: true>
+
+      iex> params = %{"info_hash" => "f0a15e27fafbffc1c2f18f69fcac2dfa461ff4e8"}
+      iex> YaBTT.Schema.Params.changeset(%YaBTT.Schema.Params{}, params)
+      #Ecto.Changeset<action: nil, changes: %{info_hash: \"f0a15e27fafbffc1c2f18f69fcac2dfa461ff4e8\"}, errors: [peer_id: {"can't be blank", [validation: :required]}], data: #YaBTT.Schema.Params<>, valid?: false>
+  """
   @spec changeset(changeset_t() | t(), params()) :: changeset_t()
   def changeset(struct, params) do
     struct
@@ -22,7 +50,22 @@ defmodule YaBTT.Schema.Params do
     |> validate_required([:info_hash, :peer_id])
   end
 
-  @doc false
+  @doc """
+  validates the `params` and returns a `YaBTT.Schema.Params` with `:insert` action.
+
+  ## Parameters
+
+  - `params`: the request parameters
+
+  ## Examples
+
+      iex> params = %{"info_hash" => "f0a15e27fafbffc1c2f18f69fcac2dfa461ff4e8", "peer_id" => "-TR14276775888084598"}
+      iex> YaBTT.Schema.Params.apply(params)
+      {:ok, %YaBTT.Schema.Params{info_hash: "f0a15e27fafbffc1c2f18f69fcac2dfa461ff4e8", peer_id: "-TR14276775888084598"}}
+
+      iex> params = %{"info_hash" => "f0a15e27fafbffc1c2f18f69fcac2dfa461ff4e8"}
+      iex> {:error, _} =  YaBTT.Schema.Params.apply(params)
+  """
   @spec apply(params()) :: {:ok, t()} | {:error, changeset_t()}
   def apply(params), do: changeset(%__MODULE__{}, params) |> apply_action(:insert)
 end
--- test/yabtt/schema/peer_test.exs
+++ test/yabtt/schema/peer_test.exs
@@ -1,5 +1,5 @@
 defmodule YaBTT.Schema.PeerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   doctest YaBTT.Schema.Peer
   doctest Bento.Encoder.YaBTT.Schema.Peer
