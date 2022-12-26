e6bdc4dd468ed5fa14d60f97dce8bdcc23aa04be
Mogeko <zhengjunyi@live.com>
2022-12-26 23:32:04 +0000

    build: setup ex_doc

--- mix.exs
+++ mix.exs
@@ -1,19 +1,25 @@
 defmodule YaBTT.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+  @source_url "https://github.com/mogeko/yabtt"
+
   def project do
     [
       app: :yabtt,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      deps: deps(),
       test_coverage: test_coverage(),
       aliases: aliases(),
-      deps: deps()
+
+      # Docs
+      name: "YaBTT",
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -21,15 +27,15 @@ defmodule YaBTT.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:benchee, "~> 1.1.0", only: :dev},
       {:benchee_html, "~> 1.0", only: :dev},
-      {:plug_cowboy, "~> 2.6.0"},
+      {:bento, "~> 0.9"},
       {:ecto_sql, "~> 3.8"},
       {:ecto_sqlite3, "~> 0.8"},
-      {:bento, "~> 0.9"}
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+      {:plug_cowboy, "~> 2.6.0"}
     ]
   end
 
@@ -45,6 +51,14 @@ defmodule YaBTT.MixProject do
     [
       ignore_modules: [YaBBT.Application, YaBTT.Repo],
       summary: [threshold: 85]
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md", "LICENSE"],
+      source_url: @source_url
     ]
   end
 end
