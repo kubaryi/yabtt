4aaeef80b37dcdf8b3ffc6e4db1476ec0759eade
Mogeko <zhengjunyi@live.com>
2022-12-27 19:12:55 +0000

    build: change the location of db for non-prod mode

--- config/config.exs
+++ config/config.exs
@@ -2,12 +2,10 @@ import Config
 
 config :yabtt, ecto_repos: [YaBTT.Repo]
 
-config :yabtt, YaBTT.Repo, database: "./_build/data/yabtt.#{config_env()}.db"
+config :yabtt, YaBTT.Repo, database: "./_build/#{config_env()}/yabtt.db"
 
 if config_env() == :test do
-  config :yabtt, YaBTT.Repo,
-    database: "./_build/data/yabtt.#{config_env()}.db",
-    pool: Ecto.Adapters.SQL.Sandbox
+  config :yabtt, YaBTT.Repo, pool: Ecto.Adapters.SQL.Sandbox
 end
 
 if config_env() == :prod do
