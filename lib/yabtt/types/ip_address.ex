48b2337f2be2b2f5e34c06c0b9b7bcb81d101eec
Mogeko <zhengjunyi@live.com>
2022-12-30 18:48:36 +0000

    test: add test for ip_adderss.ex

--- lib/yabtt/types/ip_address.ex
+++ lib/yabtt/types/ip_address.ex
@@ -1,15 +1,47 @@
 defmodule YaBTT.Types.IPAddress do
-  @moduledoc false
+  @moduledoc """
+  A custom type for IP addresses.
+  """
 
   use Ecto.Type
 
   @type ip_addr :: :inet.ip_address()
 
-  @doc false
+  @doc """
+  Returns the underlying schema type for the custom type.
+  """
   @spec type :: :binary
   def type, do: :binary
 
-  @doc false
+  @doc """
+  Casts the given value to an IP address (`ip_addr()`).
+
+  There are two situations where this callback is called:
+
+  1. When casting values by Ecto.Changeset
+  2. When passing arguments to Ecto.Query
+
+  It will return `:error` if the given term cannot be cast.
+
+  ## Parameters
+
+    * `ip` - The IP address to cast. The Ip address could be a binary, a
+      charlist, or an `ip_addr()`.
+
+  ## Examples
+
+      iex> YaBTT.Types.IPAddress.cast('127.0.0.1')
+      {:ok, {127, 0, 0, 1}}
+
+      iex> YaBTT.Types.IPAddress.cast("::1")
+      {:ok, {0, 0, 0, 0, 0, 0, 0, 1}}
+
+      iex> YaBTT.Types.IPAddress.cast({127, 0, 0, 1})
+      {:ok, {127, 0, 0, 1}}
+
+      iex> YaBTT.Types.IPAddress.cast("abc")
+      :error
+  """
   @spec cast(binary() | charlist()) :: :error | {:ok, ip_addr()}
   def cast(ip) when is_binary(ip), do: cast(to_charlist(ip))
 
@@ -25,11 +57,49 @@ defmodule YaBTT.Types.IPAddress do
   def cast({_, _, _, _, _, _, _, _} = ipv6), do: {:ok, ipv6}
   def cast(_), do: :error
 
-  @doc false
+  @doc """
+  Loads the IP address from the database.
+
+  This callback is called when loading values from the database.
+
+  It will return `:error` if the given term cannot be loaded.
+
+  ## Parameters
+
+    * `ip` - The IP address to load from the database. It is should be a
+      binary in the normal case.
+
+  ## Examples
+
+      iex> YaBTT.Types.IPAddress.load("127.0.0.1")
+      {:ok, {127, 0, 0, 1}}
+  """
   @spec load(binary()) :: :error | {:ok, ip_addr()}
   def load(ip), do: cast(ip)
 
-  @doc false
+  @doc """
+  Dumps the IP address to the database.
+
+  This callback is called when dumping values to the database.
+
+  It will return `:error` if the given term cannot be dumped.
+
+  ## Parameters
+
+    * `ip` - The IP address to dump to the database. It should be an
+      `ip_addr()`.
+
+  ## Examples
+
+      iex> YaBTT.Types.IPAddress.dump({127, 0, 0, 1})
+      {:ok, "127.0.0.1"}
+
+      iex> YaBTT.Types.IPAddress.dump({0, 0, 0, 0, 0, 0, 0, 1})
+      {:ok, "::1"}
+
+      iex> YaBTT.Types.IPAddress.dump("abc")
+      :error
+  """
   @spec dump(ip_addr()) :: :error | {:ok, binary()}
   def dump({_, _, _, _} = ipv4), do: {:ok, do_dump(ipv4)}
   def dump({_, _, _, _, _, _, _, _} = ipv6), do: {:ok, do_dump(ipv6)}
