877d97ef4c6c57d204c2d86a17dc32ec7f3c84f0
Mogeko <zhengjunyi@live.com>
2022-12-31 18:47:39 +0000

    feat: remove redundant agreements

--- lib/yabtt/schema/torrent.ex
+++ lib/yabtt/schema/torrent.ex
@@ -60,34 +60,4 @@ defmodule YaBTT.Schema.Torrent do
     |> validate_required([:info_hash])
     |> validate_length(:info_hash, is: 20)
   end
-
-  defimpl YaBTT.Response do
-    @moduledoc """
-    Implements the `YaBTT.Response` protocol for `YaBTT.Schema.Torrent`.
-    """
-
-    alias YaBTT.{Schema.Torrent, Response}
-
-    @doc """
-    Extracts a `YaBTT.Schema.Torrent` into a `map()`.
-    """
-    @spec extract(Torrent.t(), Response.opts()) :: map()
-    def extract(torrent, opts) do
-      %{
-        interval: Application.get_env(:yabtt, :interval, 3600),
-        peers: handle_peers(torrent.peers, opts)
-      }
-    end
-
-    @typep peers :: [Peer.t()]
-
-    @spec handle_peers(peers(), Response.opts()) :: binary() | Enumerable.t()
-    defp handle_peers(peers, opts) do
-      with {:ok, 1} <- Keyword.fetch(opts, :compact) do
-        for(peer <- peers, into: <<>>, do: Response.extract(peer, opts))
-      else
-        _ -> Stream.map(peers, &Response.extract(&1, opts))
-      end
-    end
-  end
 end
