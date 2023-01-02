fbce931e2aa445f7d35aa4b985a54e4d9fa4e43d
Mogeko <zhengjunyi@live.com>
2023-01-02 23:30:29 +0000

    feat: setup the https

--- config/config.exs
+++ config/config.exs
@@ -4,10 +4,22 @@ config :yabtt, ecto_repos: [YaBTT.Repo]
 
 config :yabtt, YaBTT.Repo, database: "./_build/#{config_env()}/yabtt.db"
 
+config :yabtt, Plug.Cowboy,
+  scheme: :https,
+  certfile: "priv/cert/selfsigned.pem",
+  keyfile: "priv/cert/selfsigned_key.pem",
+  cipher_suite: :strong,
+  otp_app: :yabtt
+
 if config_env() == :test do
   config :yabtt, YaBTT.Repo, pool: Ecto.Adapters.SQL.Sandbox
 end
 
 if config_env() == :prod do
   config :yabtt, YaBTT.Repo, database: "/var/lib/sqlite3/yabtt.db"
+
+  config :yabtt, Plug.Cowboy,
+    certfile: "/etc/yabtt/ssl/cert.pem",
+    keyfile: "/etc/yabtt/ssl/privkey.pem",
+    cacertfile: "/etc/yabtt/ssl/chain.pem"
 end
--- config/runtime.exs
+++ config/runtime.exs
@@ -4,15 +4,19 @@ config :yabtt,
   # Set the interval in seconds between each scrape, default to 3600.
   # See: https://wiki.theory.org/BitTorrentSpecification#Tracker_Response
   interval: get_env("YABTT_INTERVAL", "3600") |> to_integer(),
-  # Set the port to listen on, default to 8080.
-  port: get_env("YABTT_PORT", "8080") |> to_integer(),
   # How many peers can be returned in one query? default to 50, but we
   # recommend you to set it smaller, like 30. Because this value is
   # important to performance. Practice tells us that even 30 peers is plenty.
   # See: https://wiki.theory.org/BitTorrentSpecification#Tracker_Response
   query_limit: get_env("YABTT_QUERY_LIMIT", "50") |> to_integer(),
   # Force the use of "compact mode", default to false.
-  compact_only: get_env("YABTT_COMPACT_ONLY", "false") |> downcase() |> to_existing_atom()
+  compact_only: get_env("YABTT_COMPACT_ONLY", "false") |> downcase() |> to_existing_atom(),
+  # Disable HTTPS, default to false.
+  disable_https: get_env("YABTT_DISABLE_HTTPS", "false") |> downcase() |> to_existing_atom()
+
+config :yabtt, Plug.Cowboy,
+  # Set the port to listen on, default to 8080.
+  port: get_env("YABTT_PORT", "8080") |> to_integer()
 
 config :logger,
   # Set the log level, default to :info.
