cf5ab9ddd84b9a02f41341e4df7731daff4cecbc
Mogeko <zhengjunyi@live.com>
2023-01-18 09:53:44 +0000

    refactor: rewrite the case of doctest

--- test/yabtt/schema/peer_test.exs
+++ test/yabtt/schema/peer_test.exs
@@ -2,4 +2,70 @@ defmodule YaBTT.Schema.PeerTest do
   use ExUnit.Case, async: true
 
   doctest YaBTT.Schema.Peer
+
+  alias YaBTT.Schema.Peer
+
+  @params %{
+    "peer_id" => "-TR14276775888084598",
+    "port" => "6881"
+  }
+
+  test "changeset with valid params" do
+    changeset = Peer.changeset(%Peer{}, @params, {1, 2, 3, 4})
+
+    assert changeset.valid?
+
+    assert changeset.changes == %{
+             peer_id: "-TR14276775888084598",
+             ip: {1, 2, 3, 4},
+             port: 6881
+           }
+  end
+
+  test "changeset with explicit IP" do
+    params = Map.put(@params, "ip", "127.0.0.1")
+    changeset = Peer.changeset(%Peer{}, params, {1, 2, 3, 4})
+
+    assert changeset.valid?
+
+    assert changeset.changes == %{
+             peer_id: "-TR14276775888084598",
+             ip: {127, 0, 0, 1},
+             port: 6881
+           }
+  end
+
+  test "changeset with IPv6" do
+    params = Map.put(@params, "ip", "::1")
+    changeset = Peer.changeset(%Peer{}, params, {1, 2, 3, 4})
+
+    assert changeset.valid?
+
+    assert changeset.changes == %{
+             peer_id: "-TR14276775888084598",
+             ip: {0, 0, 0, 0, 0, 0, 0, 1},
+             port: 6881
+           }
+  end
+
+  test "changeset with invalid params" do
+    changeset = Peer.changeset(%Peer{}, %{}, {1, 2, 3, 4})
+
+    assert not changeset.valid?
+
+    assert changeset.errors == [
+             peer_id: {"can't be blank", [validation: :required]},
+             port: {"can't be blank", [validation: :required]}
+           ]
+  end
+
+  test "changeset with invalid ip" do
+    changeset = Peer.changeset(%Peer{}, @params, {1, 2, 3, 4, 5})
+
+    assert not changeset.valid?
+
+    assert changeset.errors == [
+             ip: {"is invalid", [{:type, YaBTT.Types.IPAddress}, {:validation, :cast}]}
+           ]
+  end
 end
--- test/yabtt/schema/torrent_test.exs
+++ test/yabtt/schema/torrent_test.exs
@@ -2,4 +2,25 @@ defmodule YaBTT.Schema.TorrentTest do
   use ExUnit.Case, async: true
 
   doctest YaBTT.Schema.Torrent
+
+  alias YaBTT.Schema.Torrent
+
+  @params %{
+    "info_hash" =>
+      <<18, 52, 86, 120, 154, 188, 222, 241, 35, 69, 103, 137, 171, 205, 239, 18, 52, 86, 12, 15>>
+  }
+
+  test "changeset with valid params" do
+    changeset = Torrent.changeset(%Torrent{}, @params)
+
+    assert changeset.valid?
+    assert changeset.changes == %{info_hash: @params["info_hash"]}
+  end
+
+  test "changeset with invalid params" do
+    changeset = Torrent.changeset(%Torrent{}, %{})
+
+    refute changeset.valid?
+    assert changeset.errors == [info_hash: {"can't be blank", [validation: :required]}]
+  end
 end
